04815d37f8cb2dad8ac3c94c8f16d4d76ace4899
Merly Monsalve <merlysmonsalve@gmail.com>
2016-06-06 16:45:03 -0400

    nueva modificacion en btnGuardarAlumno

--- MatriculaZk/src/controlador/ControladorAlumno.java
+++ MatriculaZk/src/controlador/ControladorAlumno.java
@@ -206,6 +206,7 @@ public class ControladorAlumno extends ControladorMatricula{
 	public void guardarAlumno(){
 		Messagebox.show("Prueba de resolución de conflicto");
 		Messagebox.show("hOLA Soy yo");	
+		Messagebox.show("Si soy yo el conflicto que no se como vas a resolver");
 	}
 
  
